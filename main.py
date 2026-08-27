84663131ed5fdd7c298d425ea05a3ed80168ac12
Batte Kathyayeni <bkathyayeni05@gmail.com>
2026-08-27 16:00:40 +0530

    Fix backend CORS

--- main.py
+++ main.py
@@ -45,7 +45,10 @@ app = FastAPI(title="AI Healthcare Platform")
 # -----------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # allow all for now (safe for dev)
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
